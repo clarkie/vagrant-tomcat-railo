15bf51d9d59a5ee6bfb6206f3c78db3b7e8b6a75
Clarkie <andrew.t.clarke@gmail.com>
2013-11-05 16:15:13 +0000

    remove the railo.war
    get puppet to download the war file from
    http://www.getrailo.org/index.cfm/download/

--- manifests/railo-conf.pp
+++ manifests/railo-conf.pp
@@ -29,51 +29,11 @@ file { 'catalina.properties':
 	notify	=> Service['tomcat7'],
 }
 
-file { 'railo-dir':
-	name	=> '/var/lib/tomcat7/webapps/railo',
-	ensure	=> 'directory',
-	owner	=> 'tomcat7',
-	group	=> 'tomcat7',
-}
-
-file { 'web-inf-dir':
-	name	=> '/var/lib/tomcat7/webapps/railo/WEB-INF',
-	ensure	=> 'directory',
-	owner	=> 'tomcat7',
-	group	=> 'tomcat7',
-	require	=> File['railo-dir'],
-}
-
-file { 'lib-dir':
-	name	=> '/var/lib/tomcat7/webapps/railo/WEB-INF/lib',
-	ensure	=> 'directory',
-	owner	=> 'tomcat7',
-	group	=> 'tomcat7',
-	require	=> File['web-inf-dir'],
-}
-
-file { 'railo-server-dir':
-	name	=> '/var/lib/tomcat7/webapps/railo/WEB-INF/lib/railo-server',
-	ensure	=> 'directory',
-	owner	=> 'tomcat7',
-	group	=> 'tomcat7',
-	require	=> File['lib-dir'],
-}
-
-file { 'context-dir':
-	name	=> '/var/lib/tomcat7/webapps/railo/WEB-INF/lib/railo-server/context',
-	ensure	=> 'directory',
-	owner	=> 'tomcat7',
-	group	=> 'tomcat7',
-	require	=> File['railo-server-dir'],
-}
-
 file { 'railo-server.xml':
 	name	=> '/var/lib/tomcat7/webapps/railo/WEB-INF/lib/railo-server/context/railo-server.xml',
 	owner	=> 'tomcat7',
 	group	=> 'tomcat7',
 	source	=> '/vagrant/conf/railo-server.xml',
 	mode  => 644,
-	require	=> File['context-dir'],
 	notify	=> Service['tomcat7'],
 }
--- manifests/railo.pp
+++ manifests/railo.pp
@@ -1,68 +1,83 @@
 # railo.pp
 
-exec { 'apt-get':
-	command	=> '/usr/bin/apt-get update',
+/* define the railo version you want here: */
+$railoVersion = "4.1.1.009"
+$warFileName = "railo-${railoVersion}.war"
+
+/* update all packages */
+exec { "apt-get":
+	command	=> "/usr/bin/apt-get update",
 }
 
 /* get htop */
-package { 'htop': 
+package { "htop": 
 	ensure	=> present,
-	require	=> Exec['apt-get'],
+	require	=> Exec["apt-get"],
 }
 
-package { 'curl': 
+package { "curl": 
 	ensure	=> present,
-	require	=> Exec['apt-get'],
+	require	=> Exec["apt-get"],
 }
 
 /* add cp2 to to the hosts file */
-host { 'cp2.retailcloud.net':
-	ip	=> '127.0.0.1',
+host { "cp2.retailcloud.net":
+	ip	=> "127.0.0.1",
 }
 
 
 /* nginx proxy pointing to tomcat on 8080 */
-class { 'nginx': }
-nginx::resource::upstream { 'railo':
+class { "nginx": }
+nginx::resource::upstream { "railo":
 	ensure	=> present,
 	members	=> [
-		'localhost:8080',
+		"localhost:8080",
 	],
 }
 
 /* nginx vhost using proxy + self signed ssl */
-nginx::resource::vhost { 'cp2.retailcloud.net':
+nginx::resource::vhost { "cp2.retailcloud.net":
 	ensure		=> present,
-	proxy		=> 'http://railo',
+	proxy		=> "http://railo",
 	ssl			=> true,
-	ssl_cert	=> '/vagrant/conf/server.crt',
-	ssl_key		=> '/vagrant/conf/server.key',
+	ssl_cert	=> "/vagrant/conf/server.crt",
+	ssl_key		=> "/vagrant/conf/server.key",
 	ssl_port	=> 443,
 }
 
-/* step 1 - make sure tomcat is installed as a default */
-package {'tomcat7':
+/* make sure tomcat is installed */
+package {"tomcat7":
 	ensure	=> installed,
-	require	=> Package['curl'],
+	require	=> Package["curl"],
 }
 
-/* once tomcat is installed, update the server.xml, web.xml and catalina.properties */
-exec { 'curl':
-	name		=> '/usr/bin/curl localhost:8080',
-	require		=> File['railo.war'],
-	notify		=> Service['tomcat7'],
+/* once tomcat is installed */
+service { "tomcat7":
+	ensure  => "running",
+	enable  => "true",
+	require => Package["tomcat7"],
 }
 
-service { 'tomcat7':
-	ensure  => 'running',
-	enable  => 'true',
+/* download the railo war */
+exec { "railo-download": 
+  command => "/usr/bin/wget --quiet http://www.getrailo.org/railo/remote/download/${railoVersion}/custom/all/${warFileName}",
+  cwd => "/home/vagrant/",
+  creates => "/home/vagrant/${warFileName}",
+  require => Package['tomcat7'],
 }
 
 /* stick the railo war file into tomcat */
-file { 'railo.war':
-	name	=> '/var/lib/tomcat7/webapps/railo.war',
-	owner	=> 'tomcat7',
-	group	=> 'tomcat7',
-	source	=> '/vagrant/conf/railo.war',
-	require	=> Package['tomcat7'],
+file { "railo.war":
+	name	=> "/var/lib/tomcat7/webapps/railo.war",
+	owner	=> "tomcat7",
+	group	=> "tomcat7",
+	source	=> "/home/vagrant/${warFileName}",
+	require	=> Exec["railo-download"],
+}
+
+/* prod tomcat */
+exec { "curl":
+	name		=> "/usr/bin/curl localhost:8080",
+	require		=> File["railo.war"],
+	notify		=> Service["tomcat7"],
 }
